fc53755696a8513394ad8d46f3bc1cbb52ced729
hcdev0127 <heraclesdev@outlook.com>
2021-06-02 10:58:49 -0700

    set build env

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Builder from './pages/builder'
+import Builder from '@/pages/builder'
 
 import './App.scss';
 import './static/linearicons.css';
 import './static/stroke.css';
 import './static/averta.css';
 import './static/style.css';
-import './static/my.custom.css';
+// import './static/my.custom.css';
 import './static/tabler-icons.css';
 
 function App() {
